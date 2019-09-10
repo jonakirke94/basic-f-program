d84efa140938b3c19ffb89e9f17d5b0248e99073
Jonathan <jonakirke94@gmail.com>
2019-09-10 11:22:15 +0200

    lists

--- Program.fs
+++ Program.fs
@@ -39,3 +39,43 @@ let fibRes = fib(11)
 printf "sumBetween %A\n" fibRes
 
 
+// Lists
+let sampleList = [1..10]
+printfn "sampleList is %A" (sampleList)
+
+
+let rec product list =
+   match list with
+   | head :: tail -> head * product tail
+   | [] -> 1   
+let productRes = product(sampleList)
+printfn "product %A\n" (productRes)
+
+
+let rec matchGreaterThanFiveAmount(n) = 
+    match n with
+    | [] -> 1
+    | _ -> if n.Head > 5 then 1 + matchGreaterThanFiveAmount(n.Tail) else matchGreaterThanFiveAmount(n.Tail)
+let greaterThanFiveRes = matchGreaterThanFiveAmount(sampleList)
+printfn "greaterThanFive %A\n" (greaterThanFiveRes)
+    
+//let rec doubleAllElements(n) = 
+//    match n with
+//    | head :: tail -> head * 2 :: doubleAllElements(tail)
+//    | [] -> []
+
+let rec doubleEveryElement(list) = 
+    List.map (fun x -> x * 2) list
+let doubleEveryElementRes = doubleEveryElement(sampleList)
+printfn "greaterThanFive %A\n" (doubleEveryElement)
+
+
+let rec doublesEven(list) = 
+    List.map (fun x -> if x % 2 = 0 then x * 2 else x) list
+let doublesEvenRes = doublesEven(sampleList)
+printfn "doublesEven %A\n" (doublesEvenRes)
+
+
+
+
+
